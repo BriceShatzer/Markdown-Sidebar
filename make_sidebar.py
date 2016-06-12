4a2b3b0765ea12f52d31418f36b4d4a2d0a9dc19
Brice Shatzer <brice.shatzer@gmail.com>
2016-06-12 18:47:34 -0500

    refactored creation of display string

--- make_sidebar.py
+++ make_sidebar.py
@@ -7,13 +7,15 @@ def checkForWikiPage(file):
     else: 
         return True 
 
-def displayStr(string):
+def displayString(string):
+    if '/' in string: 
+        string = string[string.rfind('/')+1 : -3]
     return string.replace('-',' ')
 
 def createIndexFile(startpath, indexFile):
     for root, dirs, files in os.walk(startpath):
         files = [join(root,f) for f in files if not f[0] == '.']
-        #dirs[:] = [d for d in dirs if not d[0] == '.'] #dirs values aren't used
+        dirs[:] = [d for d in dirs if not d[0] == '.']
         level = root.replace(startpath, '').count(os.sep) - 1
         indent = ' ' * 2 * (level)
 
@@ -22,14 +24,14 @@ def createIndexFile(startpath, indexFile):
             for f in files:
                 if checkForWikiPage(f):                               
                     path = '/' + os.getcwd().split(os.sep)[-1] + f[1:-3]
-                    indexFile.write('- [{}]({})\n'.format( displayStr(f[f.rfind('/')+1 : -3]), path) )
+                    indexFile.write('- [{}]({})\n'.format( displayString(f), path) )
         else:
-            indexFile.write('{}- {}\n'.format( indent, displayStr(os.path.basename(root)) ) )
+            indexFile.write('{}- {}\n'.format( indent, displayString(os.path.basename(root)) ) )
             subindent = ' ' * 2 * (level + 1)
             for f in files:
                 if checkForWikiPage(f):
                     path = '/' + os.getcwd().split(os.sep)[-1] + f[1:-3]
-                    indexFile.write('{}- [{}]({})\n'.format( subindent, displayStr(f[f.rfind('/')+1 : -3]), path) )
+                    indexFile.write('{}- [{}]({})\n'.format( subindent, displayString(f), path) )
 
 indexFile = open('_Sidebar.md', 'w')
 
